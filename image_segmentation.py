bd40dad4ff66c6661f44cbb3c55eb3bffe5b849c
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-23 23:38:35 +0900

    Update image_segmentation.py

--- image_segmentation.py
+++ image_segmentation.py
@@ -5,7 +5,7 @@ from imread_from_url import imread_from_url
 
 
 encoder_model_path = "models/sam2_hiera_base_plus_encoder.onnx"
-decoder_model_path = "models/sam2_hiera_base_plus_decoder.onnx"
+decoder_model_path = "models/decoder.onnx"
 
 img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Racing_Terriers_%282490056817%29.jpg/1280px-Racing_Terriers_%282490056817%29.jpg"
 img = imread_from_url(img_url)
